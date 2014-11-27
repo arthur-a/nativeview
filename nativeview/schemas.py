8917a6baa747893b4251c74356d7a28927df4a5b
Arthur Aminov <aminov.a.r@gmail.com>
2014-11-27 17:40:29 +0500

    Sync implementation for SequenceSchema class.

--- nativeview/schemas.py
+++ nativeview/schemas.py
@@ -8,9 +8,7 @@ __all__ = ['MappingSchema', 'ObjectMappingSchema', 'SequenceSchema']
 
 # Mapping schema
 
-class MappingSchema(SchemaUnit):
-    schema_type = Mapping
-
+class MappingSyncMixin(object):
     def sync(self, instance=None, value=None):
         if instance is None:
             instance = self.instance
@@ -24,7 +22,11 @@ class MappingSchema(SchemaUnit):
             self.children[name].sync(instance, value)
 
 
-class ObjectMappingSchema(SchemaUnit):
+class MappingSchema(MappingSyncMixin, SchemaUnit):
+    schema_type = Mapping
+
+
+class ObjectMappingSchema(MappingSyncMixin, SchemaUnit):
     schema_type = ObjectMapping
 
 
@@ -36,3 +38,23 @@ class SequenceSchema(SchemaUnit):
         if len(self.children) != 1:
             raise TypeError(
                 'Sequence schemas must have exactly one child unit')
+
+    def sync(self, instance=None, value=None):
+        if instance is None:
+            instance = self.instance
+
+        if value is None:
+            value = self.validated_data
+
+        assert instance is not None, "Cannot sync with None value."
+
+        if isinstance(instance, list):
+            del instance[:]
+            instance.extend(value)
+        else:
+            instance_seq = getattr(instance, self.name, None)
+            if instance_seq is None:
+                setattr(instance, self.name, instance)
+            else:
+                del instance_seq[:]
+                instance_seq.extend(value)
