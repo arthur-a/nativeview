186d329b32eb59ee518459f685cba8244e51d08d
Arthur Aminov <aminov.a.r@gmail.com>
2015-10-26 10:18:01 +0500

    Refactor schema classes.

--- nativeview/schemas.py
+++ nativeview/schemas.py
@@ -4,65 +4,73 @@ from unit_types import Mapping, ObjectMapping, Sequence
 from units import empty
 
 
-__all__ = ['MappingSchema', 'ObjectMappingSchema', 'SequenceSchema']
+__all__ = [
+    'SyncedSchemaUnit', 'MappingSchema', 'ObjectMappingSchema',
+    'SequenceSchema'
+]
 
 
 # Mapping schema
 
-class SchemaBase(object):
-    def restore_object(self, validated_data):
+class SyncedSchemaUnit(SchemaUnit):
+    def sync(self, instance=None, value=empty):
+        """
+        Bind value data to instance, also may create a new instance
+        if passed instance is None.
+
+        Args:
+            instance - Existing instance, all data will be bind to the instance.
+            value - A validated data to bind to the instance.
+        """
         raise NotImplementedError
 
-    def sync(self, instance=None, value=None):
-        raise NotImplementedError
 
-    def sync_impl(self, instance, value, callback):
+class MappingSchema(SyncedSchemaUnit):
+    schema_type = Mapping
+
+    def sync(self, instance=None, value=empty):
+        # Everytime create a new dict
+        instance = {}
+
         if value is empty:
             value = self.validated_data
 
-        if instance is None:
-            instance = self.source_object
+        assert value is not empty, 'Cannot process empty value'
 
-        if instance is None:
-            instance = self.restore_object(value)
-
-        callback(instance, value)
+        for name, subval in value.iteritems():
+            child = self.children[name]
+            if subval is not empty and isinstance(child, SyncedSchemaUnit):
+                subinstance = instance.get(child.name)
+                subval = child.sync(instance=subinstance, value=subval)
+            instance[child.name] = subval
 
         return instance
 
 
-class MappingSchema(SchemaBase, SchemaUnit):
-    schema_type = Mapping
-
-    def restore_object(self, validated_data):
-        return {}
-
-    def sync(self, instance=None, value=empty):
-        def callback(instance, value):
-            for name, subval in value.iteritems():
-                child = self.children[name]
-                if subval is not None and isinstance(child, SchemaBase):
-                    subval = child.sync(value=subval)
-                instance[child.name] = subval
-
-        return self.sync_impl(instance, value, callback)
-
-
-class ObjectMappingSchema(SchemaBase, SchemaUnit):
+class ObjectMappingSchema(SyncedSchemaUnit):
     schema_type = ObjectMapping
 
     def sync(self, instance=None, value=empty):
-        def callback(instance, value):
-            for name, subval in value.iteritems():
-                child = self.children[name]
-                if subval is not None and isinstance(child, SchemaBase):
-                    subval = child.sync(value=subval)
-                setattr(instance, child.name, subval)
+        if instance is None:
+            instance = self.source_object
 
-        return self.sync_impl(instance, value, callback)
+        if value is empty:
+            value = self.validated_data
+
+        assert instance is not None, 'Cannot sync with None value'
+        assert value is not empty, 'Cannot process empty value'
+
+        for name, subval in value.iteritems():
+            child = self.children[name]
+            if subval is not empty and isinstance(child, SyncedSchemaUnit):
+                subinstance = getattr(instance, child.name, None)
+                subval = child.sync(instance=subinstance, value=subval)
+            setattr(instance, child.name, subval)
+
+        return instance
 
 
-class SequenceSchema(SchemaBase, SchemaUnit):
+class SequenceSchema(SyncedSchemaUnit):
     schema_type = Sequence
 
     def __init__(self, *args, **kw):
@@ -71,16 +79,19 @@ class SequenceSchema(SchemaBase, SchemaUnit):
             raise TypeError(
                 'Sequence schemas must have exactly one child unit')
 
-    def restore_object(self, validated_data):
-        return []
-
     def sync(self, instance=None, value=empty):
-        def callback(instance, value):
-            child = self.children.values()[0]
-            for i in xrange(len(value)):
-                subval = value[i]
-                if subval is not None and isinstance(child, SchemaBase):
-                    subval = child.sync(value=subval)
-                instance.append(subval)
+        # Everytime create a new list
+        instance = []
 
-        return self.sync_impl(instance, value, callback)
+        if value is empty:
+            value = self.validated_data
+
+        assert value is not empty, 'Cannot process empty value'
+
+        child = self.children.values()[0]
+        for subval in value:
+            if subval is not empty and isinstance(child, SyncedSchemaUnit):
+                subval = child.sync(value=subval)
+            instance.append(subval)
+
+        return instance
